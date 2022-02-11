0d76b010e99d111b3774508fe83f83f1356fd353
ExtemtedGold1 <wiktorbielecki31@gmail.com>
2022-02-11 11:20:33 +0100

    vcbhb

---
+++ 12945_egzamin_asp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _12945_egzamin_asp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 12945_egzamin_asp/Models/ModelTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _12945_egzamin_asp.Models
+{
+    public class ModelTask
+    {
+        public int Indeks { get; set; }
+        public string Trasa { get; set; }
+        public int Spalanie { get; set; }
+        public int Odległość { get; set; }
+        public decimal Kwota { get; set; }
+    }
+
+}
